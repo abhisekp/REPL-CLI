545fc968004247de7c5e470a1efd5ff0536861fe
Abhisek Pattnaik <abhisekp@engineer.com>
2019-10-31 20:46:02 +0530

    Format source code

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,35 @@
-import repl, { REPLServer } from 'repl';
+import repl from 'repl';
 
 const prompt = 'CMD> ';
 
-const processCMD = (cmd: string) => {
-    return cmd;
-}
+const processCMD = (cmd: string) => cmd;
 
 const evaluator = (cmd: string, ctx: object, filename: string, cb: Function) => {
-    const output = processCMD(cmd);
-    cb(null, output);
-}
+  const output = processCMD(cmd);
+  cb(null, output);
+};
 
-const outputWriter = (output: string) => {
-    return output.toUpperCase();
-}
+const outputWriter = (output: string) => output.toUpperCase();
 
 const completer = (line: string) => {
-    const cmds = ['park', 'leave', 'display'];
-    const completions = cmds.filter(cmd => cmd.startsWith(line));
+  const cmds = ['park', 'leave', 'display'];
+  const completions = cmds.filter((cmd) => cmd.startsWith(line));
 
-    return [completions, line];
-}
+  return [completions, line];
+};
 
 const replServer = repl.start({
-    prompt,
-    completer,
-    terminal: true,
-    eval: evaluator,
-    writer: outputWriter
-})
+  prompt,
+  completer,
+  terminal: true,
+  eval: evaluator,
+  writer: outputWriter,
+});
 
 replServer.on('end', () => {
-    process.exit();
+  process.exit();
 });
 
 replServer.on('exit', () => {
-    process.exit();
+  process.exit();
 });
