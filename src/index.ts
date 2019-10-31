5b5f362e3696be3d66a2c8deceb6308144dd3f0b
Abhisek Pattnaik <abhisekp@engineer.com>
2019-10-31 19:10:43 +0530

    Add Node REPL Server Demo

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import repl, { REPLServer } from 'repl';
+
+const prompt = 'CMD> ';
+
+const processCMD = (cmd: string) => {
+    return cmd;
+}
+
+const evaluator = (cmd: string, ctx: object, filename: string, cb: Function) => {
+    const output = processCMD(cmd);
+    cb(null, output);
+}
+
+const outputWriter = (output: string) => {
+    return output.toUpperCase();
+}
+
+const completer = (line: string) => {
+    const cmds = ['park', 'leave', 'display'];
+    const completions = cmds.filter(cmd => cmd.startsWith(line));
+
+    return [completions, line];
+}
+
+const replServer = repl.start({
+    prompt,
+    completer,
+    terminal: true,
+    eval: evaluator,
+    writer: outputWriter
+})
+
+replServer.on('end', () => {
+    process.exit();
+});
+
+replServer.on('exit', () => {
+    process.exit();
+});
